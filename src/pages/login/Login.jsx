16966940d04f186aaa7c4872e3e942d1fe7ba163
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2022-05-21 23:37:00 +0100

    initial commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import logo from '../../images/loginImg.png';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import FormInput from "../../components/formInput/FormInput";
+import { login } from "../../redux/apiCalls";
+import './Login.css'
+
+const Login = () => {
+
+    const [values, setValues] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+    const dispatch = useDispatch();
+
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        login(dispatch,{...values})
+    };
+
+    const onChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value})
+    }
+
+    const inputs = [
+        /*{
+          id: 1,
+          name: "firstname",
+          type: "text",
+          placeholder: "First Name",
+          errorMessage:
+            "",
+          label: "First Name",
+          pattern: "^[A-Za-z0-9]{3,16}$",
+          required: true,
+        },*/
+        {
+          id: 2,
+          name: "email",
+          type: "email",
+          placeholder: "Email",
+          errorMessage: "It should be a valid email address!",
+          label: "Email",
+          required: true,
+        },
+        /*{
+          id: 3,
+          name: "lastname",
+          type: "text",
+          placeholder: "Last Name",
+          errorMessage:
+            "",
+          label: "Last Name",
+          pattern: "^[A-Za-z0-9]{3,16}$",
+          required: true,
+        },*/
+        {
+          id: 4,
+          name: "password",
+          type: "password",
+          placeholder: "Password",
+          errorMessage:
+            "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+          label: "Password",
+          //pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+          required: true,
+        },
+        /*{
+          id: 5,
+          name: "confirmPassword",
+          type: "password",
+          placeholder: "Confirm Password",
+          errorMessage: "Passwords don't match!",
+          label: "Confirm Password",
+          pattern: values.password,
+          required: true,
+        },*/
+      ];
+    return (
+        <div className='login'>
+
+            <div className='loginCard'>
+                <img src={logo} alt='the decalmasters logo' />
+                <p>Dashboard</p>
+                <p>Log In to Dashboard</p>
+                <p>Enter your email and password below</p>
+                <form onSubmit={handleSubmit}>
+                    {inputs.map((input) => (
+                        <FormInput 
+                            key={input.id}
+                            {...input}
+                            value={values[input.name]}
+                            onChange={onChange}
+                        />
+                    ))}
+                    <button className='button'>Log In</button>
+                </form>
+                <p className='registerLink'>Don't have an account?<span>Sign Up</span></p>
+            </div>
+
+        </div>
+    );}
+
+export default Login;
---
+++ src/redux/apiCalls.js
@@ -0,0 +1,21 @@
+import { loginFailure, loginStart, loginSuccess, logoutStart, logoutSuccess, logoutFailure } from "./userRedux"
+import { publicRequest } from "../requestMethods";
+
+export const login = async (dispatch,user) => {
+    dispatch(loginStart());
+    try{
+        const res = await publicRequest.post('/auth/adminlogin', user)
+        dispatch(loginSuccess(res.data))
+    }catch(err){
+        dispatch(loginFailure())
+    }
+}
+
+export const logout = async (dispatch) => {
+    dispatch(logoutStart());
+    try {
+        dispatch(logoutSuccess())
+    }catch(err){
+        dispatch(logoutFailure())
+    }
+}
